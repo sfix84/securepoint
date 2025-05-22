e9f1b6e6a27a46093742a016bc4b77793248e8a8
Sascha Fix <sascha.fix@web.de>
2025-05-22 09:04:01 +0200

    Initialer commit mit der testversion der index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$logdata = file_get_contents('../updatev12-access-pseudonymized.log', false, null, 0, 100000);
+
+
+$datasplit = preg_split('/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/',$logdata);
+
+print_r($datasplit);
