443c61c7a9d92b30aa314b40b34d805f4c4b2d45
Sascha Fix <sascha.fix@web.de>
2025-05-24 10:05:45 +0200

    Jetzt werden auch die JSON-Daten dekodiert und in die DB geschrieben

--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 
 // DB connection
 try {
-    $db = new PDO("mysql:dbname=securepoint-logs;host=localhost",
-    "root",
-    "");
+    $db = new PDO(
+        "mysql:dbname=securepoint-logs;
+        host=localhost",
+        "root",
+        ""
+        );
     } catch (PDOException $e) {
-    echo 'Fehler: ' . htmlspecialchars($e->getMessage());
-    exit();
+        echo 'Fehler: ' . htmlspecialchars($e->getMessage());
+        exit();
     }
 
 // Open file pointer to log-file
-$file = fopen('../updatev12-access-pseudonymized_temp.log', 'r');
+$file = fopen('../updatev12-access-pseudonymized.log', 'r');
 
 if ($file){
 
@@ -23,7 +26,6 @@ if ($file){
         // decodation manual stackoverflow for logfiles with gzip part in it:
         // gets the part of the array, where the decoded data is
         $jpart = $logarray[12];
-        echo $jpart;
         //gets the gzip and base64 decoded json part via cutting off the "specs=" with regex
         $json = preg_match('/specs=([A-Za-z0-9+\/=]+)/', $jpart, $matches);
         // puts the decoded data in a variable: matches[0] outside the brackets(), matches [1] is the part inside the brackets
@@ -33,35 +35,108 @@ if ($file){
         //then decoding with gzip is needed
         $json = gzdecode($gzipjson);
         // creating an assoziative array
-        $specsarray = json_decode($json);
-        echo "<pre>";
-        var_dump($specsarray);
-        echo "</pre>";
-        echo "<br>--------------------------<br>";
-        echo "<pre>";
-        var_dump($logarray);
-        echo "</pre>";
-        echo "<br>--------------------------<br>";
-        
-        $ip = $logarray[0];
-        $updateserver = $logarray[1];
-        $datetime = $logarray[2];
-        $method = $logarray[3];
-        $url = $logarray[4];
-        $protocol = $logarray[5];
-        $status = $logarray[6];
-        $size = $logarray[7];
-        $reverse_proxy = $logarray[8];
-        $duration = $logarray[9];
-        $serialnumber_license = $logarray[10];
-        $firmware_version = $logarray[11];
-        $system_status = $logarray[12];
+        $specsarray = json_decode($json, true);
+                
+        // putting the values into variables for sql placeholders, with coalescent operater cause as seen in tests sometimes there is no value
+        $ip = $logarray[0] ?? NULL;
+        $updateserver = $logarray[1] ?? NULL;
+        $datetime = $logarray[2] ?? NULL;
+        $method = $logarray[3] ?? NULL;
+        $url = $logarray[4] ?? NULL;
+        $protocol = $logarray[5] ?? NULL;
+        $status = $logarray[6] ?? NULL;
+        $size = $logarray[7] ?? NULL;
+        $reverse_proxy = $logarray[8] ?? NULL;
+        $duration = $logarray[9] ?? NULL;
+        $serialnumber_license = $logarray[10] ?? NULL;
+        $firmware_version = $logarray[11] ?? NULL;
+        $system_status = $logarray[12] ?? NULL;
 
-        $stmt = $db->prepare("INSERT INTO log_data (ip, updateserver, datetime, method, url, protocol, status, size, reverse_proxy, duration,serialnumber_license, firmware_version, system_status) VALUES 
-        (:ip, :updateserver, :datetime, :method,  :url,  :protocol,  :status,  :size, :reverse_proxy,  :duration,  :serialnumber_license, :firmware_version, :system_status)
+        $mac = $specsarray["mac"] ?? NULL;
+        $architecture = $specsarray["architecture"] ?? NULL;
+        $machine = $specsarray["machine"] ?? NULL;
+        $nic = $specsarray["nic"] ?? NULL;
+        $mem = $specsarray["mem"] ?? NULL;
+        $cpu = $specsarray["cpu"] ?? NULL;
+        $disk_root = $specsarray["disk_root"] ?? NULL;
+        $disk_data = $specsarray["disk_data"] ?? NULL;
+        $uptime = $specsarray["uptime"] ?? NULL;
+        $fwversion = $specsarray["fwversion"] ?? NULL;
+        $l2tp = $specsarray["l2tp"] ?? NULL;
+        $qos = $specsarray["qos"] ?? NULL;
+        $httpaveng = $specsarray["httpaveng"] ?? NULL;
+        $spcf = $specsarray["spcf"] ?? NULL;
+        $architecture_json = $specsarray["architecture"] ?? NULL;
+        $footer = $specsarray["footer"] ?? NULL;
+
+
+        //sql part for filling the database
+        $stmt = $db->prepare("INSERT INTO log_data (
+        ip, 
+        updateserver, 
+        datetime, 
+        method, 
+        url, 
+        protocol, 
+        status, 
+        size, 
+        reverse_proxy, 
+        duration,
+        serialnumber_license, 
+        firmware_version, 
+        system_status,
+        mac,
+        architecture,
+        machine,
+        nic,
+        mem,
+        cpu,
+        disk_root,
+        disk_data,
+        uptime,
+        fwversion,
+        l2tp,
+        qos,
+        httpaveng,
+        spcf,
+        architecture_json,
+        footer
+        ) VALUES 
+        (
+        :ip, 
+        :updateserver, 
+        :datetime, 
+        :method,  
+        :url, 
+        :protocol, 
+        :status, 
+        :size, 
+        :reverse_proxy,  
+        :duration,  
+        :serialnumber_license, 
+        :firmware_version, 
+        :system_status,
+        :mac,
+        :architecture,
+        :machine,
+        :nic,
+        :mem,
+        :cpu,
+        :disk_root,
+        :disk_data,
+        :uptime,
+        :fwversion,
+        :l2tp,
+        :qos,
+        :httpaveng,
+        :spcf,
+        :architecture_json,
+        :footer
+        )
         ");
         $stmt->execute(
-            ['ip' => $ip,
+            [
+            'ip' => $ip,
             'updateserver' => $updateserver,
             'datetime' => $datetime,
             'method' => $method,
@@ -73,7 +148,24 @@ if ($file){
             'duration' => $duration,
             'serialnumber_license' => $serialnumber_license,
             'firmware_version' => $firmware_version,
-            'system_status' => $system_status]
+            'system_status' => $system_status,
+            'mac' => $mac,
+            'architecture' => $architecture,
+            'machine' => $machine,
+            'nic' => $nic,
+            'mem' => $mem,
+            'cpu' => $cpu,
+            'disk_root' => $disk_root,
+            'disk_data' => $disk_data,
+            'uptime' => $uptime,
+            'fwversion' => $fwversion,
+            'l2tp' => $l2tp,
+            'qos' => $qos,
+            'httpaveng' => $httpaveng,
+            'spcf' => $spcf,
+            'architecture_json' => $architecture_json,
+            'footer' => $footer
+            ]
         );
         
     }
